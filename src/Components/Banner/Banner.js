37d93733ef607b477cc41336c5a9c9754183f26c
rashi9551 <rashidroz1361@gmail.com>
2024-03-10 17:37:39 +0530

    Netflix-React

---
+++ src/Components/Banner/Banner.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+function Banner() {
+  return (
+    <div className=''>
+        <div className=''>
+            <h1 className=''>Movie name</h1>
+            <div className=''>
+                <button className=''>Play</button>
+                <button className=''>My List</button>
+            </div>
+            <h1 className=''></h1>
+        </div>
+    </div>
+  )
+}
+
+export default Banner
